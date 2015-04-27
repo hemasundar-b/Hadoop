d8e6ead66fcf7066f25887693c7b535ee6433f7e
cloudera <cloudera@localhost.localdomain>
2015-04-27 04:00:36 -0700

    Joins

--- training/src/Joins/MapSideJoinDistCacheDriver.java
+++ training/src/Joins/MapSideJoinDistCacheDriver.java
@@ -1,14 +1,43 @@
 package Joins;
 
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.filecache.DistributedCache;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
 public class MapSideJoinDistCacheDriver extends Configured implements Tool {
 
+	@SuppressWarnings("deprecation")
 	@Override
-	public int run(String[] arg0) throws Exception {
+	public int run(String[] args) throws Exception {
 		
-		return 0;
+		Job job = new Job(getConf(), getClass().getSimpleName());
+		job.setJarByClass(getClass());
+		job.setMapperClass(MapSideJoinDistCacheMapper.class);
+		job.setNumReduceTasks(0);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(NullWritable.class);
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		DistributedCache.addCacheFile(new Path(args[0]).toUri(), job.getConfiguration());
+		FileInputFormat.addInputPath(job, new Path(args[1]));
+		FileOutputFormat.setOutputPath(job, new Path(args[2]));
+		
+		boolean status = job.waitForCompletion(true);
+		return status ? 0 : 1;
+	}
+	
+	public static void main(String[] args) throws Exception{
+		int exitCode = ToolRunner.run(new MapSideJoinDistCacheDriver(), args);
+		System.exit(exitCode);
 	}
 
 }
--- training/src/Joins/MapSideJoinDistCacheMapper.java
+++ training/src/Joins/MapSideJoinDistCacheMapper.java
@@ -3,44 +3,73 @@ package Joins;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class MapSideJoinDistCacheMapper extends Mapper<LongWritable, Text, Employee, NullWritable> {
+
+public class MapSideJoinDistCacheMapper extends Mapper<LongWritable, Text, Text, NullWritable> {
 
 	private Integer dept;
 	private String dname;
-	private List<Dept> al = new ArrayList<Dept>();
-	
+	private String entireOutRecVar;
+	private Text entireOutRec = new Text();
+	private Map<Integer, String> deptMap = new HashMap<Integer, String>();
+	private static final Log log = LogFactory.getLog(MapSideJoinDistCacheMapper.class);
 	
 	@Override
-	protected void map(LongWritable key, Text value,Context context)
+	public void map(LongWritable key, Text value,Context context)
 			throws IOException, InterruptedException {
+		
+		String inputRec = value.toString(); 
+		Integer deptid = Integer.parseInt(inputRec.split("\\|")[2]);
+		log.info("deptid from employee file " + deptid );
+		if (deptMap.containsKey(deptid)){
+			String dnameVar = deptMap.get(deptid);
+			entireOutRecVar = inputRec + "|" + dnameVar; 
+		}else{
+			entireOutRecVar = inputRec + "|" + "";
+		}
+		entireOutRec.set(entireOutRecVar);
+		context.write(entireOutRec, NullWritable.get());
 	}
 
 	@SuppressWarnings("deprecation")
 	@Override
-	protected void setup(Context context)
+	public void setup(Context context)
 			throws IOException, InterruptedException {
-		String line = null;
-		Path[] cacheFiles = context.getLocalCacheFiles();
-		String deptFile = cacheFiles[0].toString();
-		BufferedReader br = new BufferedReader(new FileReader(deptFile));
-		while ((line = br.readLine()) != null){
-			String[] entireRecArr = line.split("\\|");
-			dept = Integer.parseInt(entireRecArr[0]);
-			dname = entireRecArr[1];
-			Dept deptObj = new Dept(dept,dname);
-			al.add(deptObj);
-			
-		}
-		br.close();
+
+			Path[] cacheFiles = context.getLocalCacheFiles();
+			for (Path cacheFile : cacheFiles){
+				String deptFile = cacheFile.toString();
+				readCacheFiles(deptFile);
+			}
 	}
 
+	
+	public void readCacheFiles(String deptFile) throws IOException{
+		
+		String line = null;
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(deptFile));
+			while ((line = br.readLine()) != null){
+				String[] entireRecArr = line.split("\\|");
+				dept = Integer.parseInt(entireRecArr[0]);
+				dname = entireRecArr[1];
+				log.info(dept + "|" + dname);
+				deptMap.put(dept, dname);
+			}
+			
+			br.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
