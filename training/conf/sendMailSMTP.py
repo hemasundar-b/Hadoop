2110fa1c7d5ed106834e8ebd265d02a949a197a4
cloudera <cloudera@localhost.localdomain>
2015-07-06 21:11:36 -0700

    sendmail

--- training/conf/sendMailSMTP.py
+++ training/conf/sendMailSMTP.py
@@ -1,9 +1,25 @@
 import smtplib
-user=username
-pass=password
+import email.utils
+from email.mime.text import MIMEText
+from email.MIMEMultipart import MIMEMultipart
+
+fromaddr = "from Email Address"
+toaddr = "to Email Address"
+msg = MIMEMultipart()
+msg['From'] = email.utils.formataddr(('Hemasundar Battina', fromaddr))
+msg['To'] = toaddr
+msg['Subject'] = "Python email"
+
+body = "Python test mail"
+msg.attach(MIMEText(body, 'plain'))
+
+user=user
+password =password
+
 server = smtplib.SMTP()
 server.connect("smtp.outlook.office365.com",587)
 server.ehlo()
 server.starttls()
-server.login(user,pass)
-server.sendmail("hemasundar.battina@exusia.com","hemasundar.battina@exusia.com","this is a testmessage")
+server.login(user,password)
+text = msg.as_string()
+server.sendmail(fromaddr,toaddr,text)
