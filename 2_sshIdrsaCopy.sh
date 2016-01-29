a5315a5cf1bfd126e0ba21593daf5e5e642c3649
hemasundar-b <hemasundar.battina@gmail.com>
2016-01-29 19:09:50 +0530

    Create 2_sshIdrsaCopy.sh

---
+++ 2_sshIdrsaCopy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+host_list=$1
+user=$2
+pass=$3
+
+## Place the public key for every server to all other servers
+## Loop through all servers in the host list file
+
+for i in $(cat $host_list)
+do
+## Loop through all servers in the host list file EXCEPT the current server being looked at in the main loop (variable i)
+        for j in $(cat $host_list | grep -v $i )
+        do
+                ### scp id_rsa.pub over to remote host
+                cmd="scp $user@${i}:$HOME/.ssh/id_rsa.pub $user@${j}:.ssh/id_rsa-${i}.pub"
+                expect <<- DONE
+                spawn $cmd
+                expect {
+                "*assword*"   { send $pass\r; exp_continue }
+                "*yes/no*"    { send "yes\r"; exp_continue }
+                "*assword*"  { send "$pass\r" ; exp_continue}
+                }
+                DONE
+        done
+done
